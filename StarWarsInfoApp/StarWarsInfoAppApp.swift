c9a12d574ce5825de38aebef7236da966a9986a0
Omar D <omardavidson227@gmail.com>
2023-10-03 17:54:01 -0400

    Initial Commit

---
+++ StarWarsInfoApp/StarWarsInfoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsInfoAppApp.swift
+//  StarWarsInfoApp
+//
+//  Created by Omar Davidson II on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsInfoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
